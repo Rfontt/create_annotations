4393bb9516623b48e8bcdd2075017c1555301b92
Rfontt <ritadecassia.infor2@gmail.com>
2024-01-06 15:59:08 -0300

    feat: add initial configs

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.project"
+version = "1.0-SNAPSHOT"
+
+val kotestVersion = "5.8.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    // Outras dependências, se necessário
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
